3b9e38babeb9ddb2d50c852ba1412e30597f8e99
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-06-01 19:50:43 -0500

    add footer

--- react-ui/src/sectioning/Footer.jsx
+++ react-ui/src/sectioning/Footer.jsx
@@ -1,10 +1,13 @@
 import React from "react"
-
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
+import './css/Footer.css'
+ 
 export default function Footer() {
   return (
     <footer>
-      <div>
-        
+      <div className="y-wrap">
+        <AiFillGithub className="icon"></AiFillGithub>
+        <AiFillLinkedin className="icon"></AiFillLinkedin>
       </div>
     </footer>
   )
